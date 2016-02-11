469b39b320a081efb6381a9a53055ce31b86b07d
Emiliano Alonzo <emialonzo@gmail.com>
2016-02-11 19:10:57 -0300

    agregando test

--- app/spec/jasmine_examples/ObtenerModeloSpec.js
+++ app/spec/jasmine_examples/ObtenerModeloSpec.js
@@ -1,39 +1,44 @@
+var intermedio = require('../../app/modeloIntermedio');
+var fs = require('fs');
+
 describe("Modelo", function() {
-  var Player = require('../../lib/jasmine_examples/Player');
-  var Song = require('../../lib/jasmine_examples/Song');
-  var player;
-  var song;
+
 
   beforeEach(function() {
     console.log();
   });
 
 
-  it("Deberia estar completo", function() {
+  it("Deberia agregar id", function() {
+    console.log("--------->" + __dirname);
+    var modelo = JSON.parse(fs.readFileSync( __dirname + "/parserOutput.txt").toString());
+    modelo = intermedio.asignarId(modelo.sentencia);
+    expect(intermedio.findById(3)).toBeDefined(); 
+    expect(intermedio.findById(9393)).not.toBeDefined();
   });
 
-  describe("when song has been paused", function() {
-    });
-
-
-  // demonstrates use of spies to intercept and test method calls
-  it("tells the current song if the user has made it a favorite", function() {
-    spyOn(song, 'persistFavoriteStatus');
-
-    player.play(song);
-    player.makeFavorite();
-
-    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-  });
-
-  //demonstrates use of expected exceptions
-  describe("#resume", function() {
-    it("should throw an exception if song is already playing", function() {
-      player.play(song);
-
-      expect(function() {
-        player.resume();
-      }).toThrowError("song is already playing");
-    });
-  });
+  // describe("when song has been paused", function() {
+  //   });
+  //
+  //
+  // // demonstrates use of spies to intercept and test method calls
+  // it("tells the current song if the user has made it a favorite", function() {
+  //   spyOn(song, 'persistFavoriteStatus');
+  //
+  //   player.play(song);
+  //   player.makeFavorite();
+  //
+  //   expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
+  // });
+  //
+  // //demonstrates use of expected exceptions
+  // describe("#resume", function() {
+  //   it("should throw an exception if song is already playing", function() {
+  //     player.play(song);
+  //
+  //     expect(function() {
+  //       player.resume();
+  //     }).toThrowError("song is already playing");
+  //   });
+  // });
 });
