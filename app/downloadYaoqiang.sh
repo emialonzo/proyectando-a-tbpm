7957ccd979cf558582dcd89d7c05cb453d4f3d32
Emiliano Alonzo <emialonzo@gmail.com>
2016-04-05 11:38:20 -0300

    descargar yaoqiang

---
+++ app/downloadYaoqiang.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+file='yaoqiang.zip'
+
+wget -O $file https://sourceforge.net/projects/bpmn/files/latest/download
+echo "Ahora debes descomprimirlo en la carpeta yaoqiang"
+# unzip -p $file | mv yaoqiang
