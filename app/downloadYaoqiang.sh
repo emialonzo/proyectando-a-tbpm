65f4f30db969e6fee94bd70c120c4b3a3372c21a
Emiliano Alonzo <emialonzo@gmail.com>
2016-04-05 12:40:40 -0300

    mejorando salida

--- app/downloadYaoqiang.sh
+++ app/downloadYaoqiang.sh
@@ -4,8 +4,8 @@ file='yaoqiang.zip'
 
 wget -O $file https://sourceforge.net/projects/bpmn/files/latest/download
 echo ""
-echo "*****************************************************"
-echo "* Ahora debes descomprimirlo en la carpeta yaoqiang *"
-echo "*****************************************************"
+echo "****************************************************"
+echo "* Ahora debes descomprimirlo en la carpeta yaoqian *"
+echo "****************************************************"
 echo ""
 # unzip -p $file | mv yaoqiang
