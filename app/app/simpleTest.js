fdce8c380777823368d235b486003ef76381d770
rodrigo <rodrigodam08@gmail.com>
2016-05-18 15:47:55 -0300

    lalalala

--- app/app/simpleTest.js
+++ app/app/simpleTest.js
@@ -30,7 +30,7 @@ function makeAllNivel(lista){
 };
 
 var path = __dirname + "/ejemplos/";;
-var nombreArchivo = "biblioteca";
+var nombreArchivo = "adjuntoInterrumpible";
 var textosPruebas = fs.readFileSync(path + nombreArchivo).toString();
 
 var modelo = procesamientoModelo.textToModel(textosPruebas);
