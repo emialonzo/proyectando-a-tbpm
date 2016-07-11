cd77e2bbe6f3e1bebbfb5e644442b934fc9da1db
Emiliano Alonzo <emialonzo@gmail.com>
2016-07-11 12:39:46 -0300

    script de yaoqiang trae la version 4

--- app/downloadYaoqiang.sh
+++ app/downloadYaoqiang.sh
@@ -4,7 +4,8 @@ file='yaoqiang.zip'
 dirname='yaoqiang'
 
 # url='localhost:8000/yaoqiang.zip'
-url='https://sourceforge.net/projects/bpmn/files/latest/download'
+# url='https://sourceforge.net/projects/bpmn/files/latest/download'
+url='https://sourceforge.net/projects/bpmn/files/4.1-GPLv3/yaoqiang-bpmn-editor-4.1.11.zip/download'
 
 wget -O $file $url
 rm -r $dirname
