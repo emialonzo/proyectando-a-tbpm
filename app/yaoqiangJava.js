f1e334ad768489d992716e11cdcdd343609d1602
Emiliano Alonzo <emialonzo@gmail.com>
2016-08-16 15:12:38 -0300

    se corrije bug en el borrado de archivos temporales

--- app/yaoqiangJava.js
+++ app/yaoqiangJava.js
@@ -3,137 +3,47 @@ var fs = require('fs');
 var x2js = require('x2js'); //new X2JS();
 var crypto = require('crypto');
 
-
 //inicializando convertirodor
 var conv = new x2js();
-
 
 //constantes
 var nombreArchivo = "salida"
 var filePathPng = __dirname + '/' + nombreArchivo + '.png';
-// var yaoqiangPath = __dirname + '/yaoqian/modules/org.yaoqiang.asaf.bpmn-graph.jar'
 var yaoqiangPath = __dirname + '/yaoqiang/modules/org.yaoqiang.bpmn.graph.jar'
 
 var filePath = __dirname + '/' + nombreArchivo + '.bpmn';
 var filePathBpmndi = __dirname + '/' + nombreArchivo + 'BPMNDI.bpmn';
 
+var error = false;
+
 var laneSet ;
 
-// bpmn str
-var yaoqiang = function(bpmn,callback, generarXml){
-  // var generarXml = generarXml || true;
-
-  //borramos laneSet por bug en yaoqiang
-  var jsonBpmn = conv.xml_str2json( bpmn );
-  laneSet = jsonBpmn.definitions.process.laneSet;
-  delete jsonBpmn.definitions.process.laneSet;
-  bpmn = conv.json2xml_str(jsonBpmn)
-
-  // console.log("bpmn:" + bpmn);
-
-  fs.writeFile(filePath, bpmn, function(err) {
-    if(err) {
-      return console.log(err);
-    }
-  });
-
-  var formato = generarXml? 'salidaBPMNDI.bpmn' : 'salida.png'
-
-  var child = require('child_process').spawn(
-    'java', ['-jar', yaoqiangPath, filePath , '--export', __dirname+'/'+formato]
-  );
-  child.stdout.on('data', function(data) {
-  });
-
-  child.stderr.on("data", function (data) {
-    var env = require('./app/env');
-    var excepcionSiFallaYaoqiang = env.excepcionSiFallaYaoqiang || false;
-    if(excepcionSiFallaYaoqiang){
-      // console.error("Error al procesar en yaoqiang");
-      throw "Error al procesar en yaoqiang"
-    }else{
-      console.log(data.toString());
-    }
-  });
-
-  child.on('close', function (code) {
-    if(generarXml){
-      //leo archivo xml
-      var xml = fs.readFileSync(filePathBpmndi).toString();
-
-      //inicializando ajuste de archivo generado
-      var jsonBpmn = conv.xml_str2json( bpmn );
-      var jsonYao = conv.xml_str2json( xml );
-      //ajustando pools
-      var listaShapes = jsonYao.definitions.BPMNDiagram.BPMNPlane.BPMNShape
-      for (var i = 0; i < listaShapes.length; i++) {
-        var shape = listaShapes[i]
-        if(shape.Bounds._width<0){
-          shape.Bounds._width = 200;
-          shape.Bounds._height = shape.BPMNLabel.Bounds._height
-          // shape.isExpanded="true"
-        }
-        listaShapes[i] = shape
-      }
-      //ajustando condiciones
-      jsonBpmn.definitions.BPMNDiagram = jsonYao.definitions.BPMNDiagram;
-      jsonBpmn.definitions.process.laneSet = laneSet;
-
-      // callback(xml);
-      callback(conv.json2xml_str(jsonBpmn))
-    }else{
-      var base64Image = fs.readFileSync(filePathPng).toString('base64');
-      callback(base64Image);
-      return base64Image;
-    }
-  });
-}
-
-var generarImagen = function(bpmn, callback){
-  try {
-    fs.unlinkSync(filePathPng);
-  } catch (e) {
-
-  }
-  yaoqiang(bpmn, callback, false);
-}
-
-var generarXml = function(bpmn, callback){
-  try {
-    fs.unlinkSync(filePathBpmndi);
-  } catch (e) {
-
-  }
-  yaoqiang(bpmn, callback, true);
-}
-
 function procesarYaoqiang(bpmn, callback){
-
+  //inicializamos nombres de archivos temporales
   var random = crypto.randomBytes(4).readUInt32LE(0);
   var filePath = __dirname + '/' + nombreArchivo + random + '.bpmn';
   var filePathBpmndi = __dirname + '/' + nombreArchivo + random + 'BPMNDI.bpmn';
 
-
   //borramos laneSet por bug en yaoqiang
   var jsonBpmn = conv.xml_str2json( bpmn );
   laneSet = jsonBpmn.definitions.process.laneSet;
   delete jsonBpmn.definitions.process.laneSet;
-
   bpmn = conv.json2xml_str(jsonBpmn)
 
-  // console.log("Y aca es "+bpmn);
-
+  //se guarda el bpmn en un archivo temporal para que pueda ser utilizado por yaoqiang
   fs.writeFile(filePath, bpmn, function(err) {
     if(err) {
       return console.log(err);
     }
   });
 
-  // console.log("filePath"+fs.readFileSync(filePath).toString());
+  //inicializamos la bandera de error en falso
+  error = false;
 
   var child = require('child_process').spawn(
     'java', ['-jar', yaoqiangPath, filePath , '--export', filePathBpmndi]
   );
+
   child.stdout.on('data', function(data) {
   });
 
@@ -144,48 +54,61 @@ function procesarYaoqiang(bpmn, callback){
     //   // console.error("Error al procesar en yaoqiang");
     //   throw "Error al procesar en yaoqiang"
     // }else{
-      console.log(data.toString());
     // }
-    try {
-      fs.unlinkSync(filePath);
-      fs.unlinkSync(filePathBpmndi);
-    } catch (e) {
-      console.log("Archivos temporales no existen.");
-    }
+    console.error(data.toString());
+    error = true;
+
   });
 
   child.on('close', function (code) {
-    var xml = fs.readFileSync(filePathBpmndi).toString();
-    // console.log("xml final:" + xml);
+    if(!error){
+      var xml = fs.readFileSync(filePathBpmndi).toString();
 
-    var jsonYao = conv.xml_str2json( xml );
+      var jsonYao = conv.xml_str2json( xml );
 
-    //ajustando pools
-    var listaShapes = jsonYao.definitions.BPMNDiagram.BPMNPlane.BPMNShape
-    for (var i = 0; i < listaShapes.length; i++) {
-      var shape = listaShapes[i];
-      if(shape.Bounds._width<0){
-        shape.Bounds._width = 200;
-        shape.Bounds._height = shape.BPMNLabel.Bounds._height
-        // shape.isExpanded="true"
+      //ajustando pools
+      var listaShapes = jsonYao.definitions.BPMNDiagram.BPMNPlane.BPMNShape
+      for (var i = 0; i < listaShapes.length; i++) {
+        var shape = listaShapes[i];
+        if(shape.Bounds._width<0){
+          shape.Bounds._width = 200;
+          shape.Bounds._height = shape.BPMNLabel.Bounds._height
+          // shape.isExpanded="true"
+        }
+        listaShapes[i] = shape;
       }
-      listaShapes[i] = shape;
-    }
-    jsonYao.definitions.BPMNDiagram.BPMNPlane.BPMNShape = listaShapes;
+      jsonYao.definitions.BPMNDiagram.BPMNPlane.BPMNShape = listaShapes;
 
-    // console.debug(jsonYao.definitions.BPMNDiagram);
-    fs.unlinkSync(filePath);
-    fs.unlinkSync(filePathBpmndi);
-    callback(jsonYao.definitions.BPMNDiagram);
+      borrarArchivosTemporales(filePath, filePathBpmndi);
+      callback(jsonYao.definitions.BPMNDiagram);
+    }else{
+      borrarArchivosTemporales(filePath, filePathBpmndi);
+    }
   });
 }
 
-var generarBpmndiJson = function(bpmn, callback){
-  var bpmndi = procesarYaoqiang(bpmn, callback);
+
+function borrarArchivosTemporales(filePath, filePathBpmndi){
+  try {
+    // console.log("Borrando archivo temporal " + filePath );
+    fs.unlinkSync(filePath);
+  } catch (e) {
+    console.log("Archivo temporal " + filePath + " no existe.");
+  }
+  try {
+    // console.log("Borrando archivo temporal " + filePathBpmndi );
+    fs.unlinkSync(filePathBpmndi);
+  } catch (e) {
+    console.log("Archivo temporal " + filePathBpmndi + " no existe.");
+  }
 }
 
+
+  var generarBpmndiJson = function(bpmn, callback){
+    var bpmndi = procesarYaoqiang(bpmn, callback);
+  }
+
+
 module.exports = {
-  generarImagen : generarImagen,
-  generarXml : generarXml,
   generarBpmndiJson : generarBpmndiJson
 };
