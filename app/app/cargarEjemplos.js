52e0cce1213621de7470c9dc2c7fe7597f37e0cc
Emiliano Alonzo <emialonzo@gmail.com>
2016-09-07 17:23:51 -0300

    depurando codigo

--- app/app/cargarEjemplos.js
+++ app/app/cargarEjemplos.js
@@ -7,16 +7,9 @@ var aux;
 
 items = fs.readdirSync(path);
 for (var i=0; i<items.length; i++) {
-  // aux = {}
-  // aux.titulo = nombre_archivo = items[i];
-  // aux.texto = fs.readFileSync(path + nombre_archivo).toString();
-  // ejemplos.push(aux)
-  // console.log(aux);
   nombre_archivo = items[i];
   ejemplos[nombre_archivo] = fs.readFileSync(path + nombre_archivo).toString();
 }
 
-// var pd = require('pretty-data').pd;
-// console.log(pd.json(ejemplos));
 
 module.exports = ejemplos;
